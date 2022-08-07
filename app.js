089f4a2ca1b3d3310b3657ae1c8cdc5bbbe56858
Robby <rizkyrobby19@gmail.com>
2022-08-07 20:31:51 +0700

    add initial get post route for login page

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
+const fs = require("fs");
 const express = require("express");
-const app = express();
-const PORT = 8000;
 const ejs = require("ejs");
 const chalk = require("chalk");
+const bodyParser = require("body-parser");
+
+const app = express();
+const jsonParser = bodyParser.json();
+
+const PORT = 8000;
 
 app.set("view engine", "ejs"); // ! using ejs view engine
 app.use(express.static("public")); // ! serving static file
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
+    let dataUser = JSON.parse(fs.readFileSync("./data-user.json"));
+    res.send(dataUser);
+});
+app.get("/home", (req, res) => {
     res.render("index");
 });
 app.get("/the-game", (req, res) => {
     res.render("the-game");
 });
+
+// ! Login Page
+//     TODO: add logic to read email and pass from user data
 app.get("/login", (req, res) => {
-    res.render("login-page");
+    res.render("login");
+});
+// * using POST method action form html
+// app.post("/login", (req, res) => {
+
+//     const ADMIN = "admin@gmail.com";
+//     const PASS = "asdf";
+//     let reqUserEmail = req.body.email;
+//     let reqUserPass = req.body.password;
+//     if (ADMIN === reqUserEmail && PASS === reqUserPass) {
+//         res.render("the-game");
+//     } else {
+//         res.status(401).send("wrong email and pass");
+//     }
+// });
+
+// ? cara dari facil
+app.post("/login", jsonParser, (req, res) => {
+    const ADMIN = "admin@gmail.com";
+    const PASS = "admin1234";
+    console.log(req.body);
+    let reqUserEmail = req.body.email;
+    console.log(reqUserEmail);
+    let reqUserPass = req.body.password;
+    console.log(reqUserPass);
+    if (ADMIN === reqUserEmail && PASS === reqUserPass) {
+        res.render("the-game");
+    } else {
+        res.status(401).send("wrong email and pass");
+    }
 });
 
 app.listen(PORT, () => {
---
+++ public/assets/javascripts/login.js
@@ -0,0 +1,25 @@
+const DATA_USER = "http://localhost:8000/users";
+const USER_LOGIN = "http://localhost:8000/login";
+
+const login = async () => {
+    let userEmail = document.getElementById("user-email").value;
+    let userPassword = document.getElementById("user-password").value;
+    console.log(userEmail);
+    console.log(userPassword);
+    let resp = await fetch(USER_LOGIN, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: userEmail,
+            password: userPassword,
+        }),
+    });
+    if (resp.status === 401) {
+        alert("Wrong Password");
+    } else {
+        alert("You are authorized");
+        window.location.replace("the-game");
+    }
+};
